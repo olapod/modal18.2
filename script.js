93aad31717e59e822aa3584f35b9eda44f0930de
Ola Podsiadlik <alecs@op.pl>
2019-02-06 18:13:37 +0100

    Final version

--- script.js
+++ script.js
@@ -4,48 +4,49 @@
       id: 1,
       title: 'Harry Potter',
       desc: 'film o czarodzieju',
-      imgUrl: 'https://www.filmweb.pl/Harry.Potter.I.Kamien.Filozoficzny/posters#prev'
+      imgUrl: 'https://cdn-images-1.medium.com/max/1600/1*FiXiocazv6hiy6YwUwKuoQ.png'
     },
     {
       id: 2,
       title: 'Król Lew',
       desc: 'Film o królu sawanny',
-      imgUrl: 'https://www.filmweb.pl/Krol.Lew/posters#next'
+      imgUrl: 'https://www.taniefototapety.eu/pol_pl_Fototapeta-3203-KROL-LEW-63427_2.jpg'
     },
     {
-        id: 3,
-        title: 'Bohemian Rhapsody',
-        desc: 'Film o zespole Queen',
-        imgUrl: 'https://ssl-gfx.filmweb.pl/po/92/01/619201/7863181.3.jpg'
+      id: 3,
+      title: 'Bohemian Rhapsody',
+      desc: 'Film o zespole Queen',
+      imgUrl: 'https://ssl-gfx.filmweb.pl/po/92/01/619201/7863181.3.jpg'
     },
     {
-        id: 4,
-        title: 'Skazani na Shawshank',
-        desc: 'Film o bankierze w więzieniu',
-        imgUrl: 'https://www.filmweb.pl/Skazani.Na.Shawshank/posters#prev'
+      id: 4,
+      title: 'Skazani na Shawshank',
+      desc: 'Film o bankierze w więzieniu',
+      imgUrl: 'https://icdn.2cda.pl/vid/premium/1127811/299x446/8eda9798a97453d135cfd18e1e960141.jpg'
     },
     {
-        id: 5,
-        title: 'Zielona mila',
-        desc: 'Film o skazanym za zabójstwo dwóch dziewczynek',
-        imgUrl: 'https://ssl-gfx.filmweb.pl/po/08/62/862/7517878.3.jpg'
-    },
+      id: 5,
+      title: 'Zielona mila',
+      desc: 'Film o skazanym za zabójstwo dwóch dziewczynek',
+      imgUrl: 'https://ssl-gfx.filmweb.pl/po/08/62/862/7517878.3.jpg'
+    }
     
   ];
-
-  var element =
+var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.imgUrl})
+    );
+  });    
+ 
+var element =
   React.createElement('div', {},
     React.createElement('h1', {}, 'Lista filmów'),
     React.createElement('ul', {}, moviesElements),
   );
 
-  var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key: movie.id},
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {src: movie.imgUrl})
-      );
-  });
+  
 
 
 ReactDOM.render(element, document.getElementById('app'));
